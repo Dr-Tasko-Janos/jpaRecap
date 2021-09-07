04b3d0c0d101feb126bfb65ba242581adf147eda
Dr-Tasko-Janos <tasko.janos@protonmail.ch>
2021-09-07 21:32:57 +0200

    Init commit

---
+++ src/main/java/employees/JpaRecapApplication.java
@@ -0,0 +1,13 @@
+package employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRecapApplication.class, args);
+    }
+
+}
